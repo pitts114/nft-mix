f1826930947759e08ff32ac72d100ee804766079
Justin Pitts <contact@justinpitts.dev>
2022-04-15 23:37:33 -0400

    reformat

--- contracts/AdvancedCollectible.sol
+++ contracts/AdvancedCollectible.sol
@@ -5,45 +5,57 @@ import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 
 contract AdvancedCollectible is ERC721, VRFConsumerBase {
     uint256 public tokenCounter;
-    enum Breed{PUG, SHIBA_INU, ST_BERNARD}
+    enum Breed {
+        PUG,
+        SHIBA_INU,
+        ST_BERNARD
+    }
     // add other things
     mapping(bytes32 => address) public requestIdToSender;
     mapping(bytes32 => string) public requestIdToTokenURI;
     mapping(uint256 => Breed) public tokenIdToBreed;
     mapping(bytes32 => uint256) public requestIdToTokenId;
-    event RequestedCollectible(bytes32 indexed requestId); 
+    event RequestedCollectible(bytes32 indexed requestId);
     // New event from the video!
     event ReturnedCollectible(bytes32 indexed requestId, uint256 randomNumber);
 
-
     bytes32 internal keyHash;
     uint256 internal fee;
-    
-    constructor(address _VRFCoordinator, address _LinkToken, bytes32 _keyhash)
-    public 
-    VRFConsumerBase(_VRFCoordinator, _LinkToken)
-    ERC721("Dogie", "DOG")
+
+    constructor(
+        address _VRFCoordinator,
+        address _LinkToken,
+        bytes32 _keyhash
+    )
+        public
+        VRFConsumerBase(_VRFCoordinator, _LinkToken)
+        ERC721("Dogie", "DOG")
     {
         tokenCounter = 0;
         keyHash = _keyhash;
-        fee = 0.1 * 10 ** 18;
+        fee = 0.1 * 10**18;
     }
 
-    function createCollectible(string memory tokenURI) 
-        public returns (bytes32){
-            bytes32 requestId = requestRandomness(keyHash, fee);
-            requestIdToSender[requestId] = msg.sender;
-            requestIdToTokenURI[requestId] = tokenURI;
-            emit RequestedCollectible(requestId);
+    function createCollectible(string memory tokenURI)
+        public
+        returns (bytes32)
+    {
+        bytes32 requestId = requestRandomness(keyHash, fee);
+        requestIdToSender[requestId] = msg.sender;
+        requestIdToTokenURI[requestId] = tokenURI;
+        emit RequestedCollectible(requestId);
     }
 
-    function fulfillRandomness(bytes32 requestId, uint256 randomNumber) internal override {
+    function fulfillRandomness(bytes32 requestId, uint256 randomNumber)
+        internal
+        override
+    {
         address dogOwner = requestIdToSender[requestId];
         string memory tokenURI = requestIdToTokenURI[requestId];
         uint256 newItemId = tokenCounter;
         _safeMint(dogOwner, newItemId);
         _setTokenURI(newItemId, tokenURI);
-        Breed breed = Breed(randomNumber % 3); 
+        Breed breed = Breed(randomNumber % 3);
         tokenIdToBreed[newItemId] = breed;
         requestIdToTokenId[requestId] = newItemId;
         tokenCounter = tokenCounter + 1;
